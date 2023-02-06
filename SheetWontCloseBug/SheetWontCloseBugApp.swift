4c38222b894649d14914e2d51312193395d1eb40
eegrok <304507+eegrok@users.noreply.github.com>
2023-02-07 12:18:08 +1300

    Initial Commit

---
+++ SheetWontCloseBug/SheetWontCloseBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetWontCloseBugApp.swift
+//  SheetWontCloseBug
+//
+//  Created by Kem Mason on 7/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SheetWontCloseBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
