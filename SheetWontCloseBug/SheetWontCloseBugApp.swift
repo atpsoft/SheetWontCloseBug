ec56a9cb65fe3266c32e0404a40a71491d3bcbc1
eegrok <304507+eegrok@users.noreply.github.com>
2023-02-07 12:44:25 +1300

    initial commit of SheetWontCloseBug

--- SheetWontCloseBug/SheetWontCloseBugApp.swift
+++ SheetWontCloseBug/SheetWontCloseBugApp.swift
@@ -1,9 +1,7 @@
-//
-//  SheetWontCloseBugApp.swift
-//  SheetWontCloseBug
-//
-//  Created by Kem Mason on 7/02/23.
-//
+// there is a bug
+// if you tap "Open Sheet"
+// then tap "Select Day"
+
 
 import SwiftUI
 
@@ -13,5 +11,67 @@ struct SheetWontCloseBugApp: App {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+struct MyItem: Identifiable {
+    let id = UUID()
+    let name: String
+}
+
+struct ContentView: View {
+  @State private var selection: MyItem? = nil
+    var body: some View {
+      VStack {
+        Button("Open Sheet") {
+          self.selection = MyItem(name: "doesn't matter")
+        }
+        .sheet(item: $selection,
+               onDismiss: { print("finished!") },
+               content: { MySheet(item: $0) })
+      
+        }
+        .padding()
+    }
+}
+
+struct MySheet: View {
+  let item: MyItem
+    @State private var day: String = ""
+    @Environment (\.presentationMode) var presentationMode
+
+    var body: some View {
+        VStack(spacing: 20) {
+          Spacer()
+          Text("Item: \(item.name)")
+          Menu(content: menuContents, label: menuLabel)
+            Text("Drag down to dismiss..., or")
+          Spacer()
+					// won't work if the "Select Day" menu is open
+          // and you're running on a device
+          // it does work in the Simulator
+            Text("Tap to Dismiss")
+                .onTapGesture {
+                    self.presentationMode.wrappedValue.dismiss()
+            }
+          Spacer()
+        }
+    }
+  @ViewBuilder func menuContents() -> some View {
+      Button("Monday") { self.day = "Monday" }
+      Button("Tuesday") { self.day = "Tuesday" }
+  }
+  
+  @ViewBuilder func menuLabel() -> some View {
+      HStack {
+          Image(systemName: "calendar")
+          Text("Select Day")
+      }
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
